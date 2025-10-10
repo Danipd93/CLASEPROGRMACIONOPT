449cb76a7d91409e3fed1e6d94aeddcf64e690d6
Danipd93 <daniperezduran93@gmail.com>
2025-10-10 16:56:13 +0200

    update

---
+++ Proyectometodos/src/Entrada.java
@@ -0,0 +1,30 @@
+public class Entrada {
+
+    public static void main(String[] args) {
+
+        System.out.println("Proyecto metodos");
+        System.out.println("Vamosa realizar llamadas a metodos");
+        saludar( "Angel");
+        saludar( "Tomas");
+        despedir();
+        int numero;
+        Operaciones operacionesMatematicas = new Operaciones();
+        operacionesMatematicas.sumar (1, 4);
+        operacionesMatematicas.restar(5, 2);
+        operacionesMatematicas.multiplicacion(5, 2);
+        operacionesMatematicas.division(20, 2);
+    }
+
+    public static void saludar(String nombre){
+    System.out.println("Hola"+nombre);
+    System.out.println("Encantado de saludarte");
+    System.out.println("vamos a praticar el llamado a metodos");
+    System.out.println("Hay que ponerle tiempo");
+    }
+
+    public static void despedir(){
+
+        System.out.println("Despedido");
+
+    }
+}
---
+++ Proyectometodos/src/Operaciones.java
@@ -0,0 +1,19 @@
+public class Operaciones {
+
+    public void sumar(int op1, int op2){
+        int resultado= op1+op2;
+        System.out.println("El resultado de la suma "+resultado);
+    }
+    public void restar(int op1, int op2){
+        int resultado= op1-op2;
+        System.out.println("El resultado de la resta "+resultado);
+    }
+    public void multiplicacion(int op1, int op2){
+        int resultado= op1*op2;
+        System.out.println("El resultado de la multiplicacion "+resultado);
+    }
+    public void division(int op1, int op2){
+        int resultado= op1/op2;
+        System.out.println("El resultado de la division "+resultado);
+    }
+}
